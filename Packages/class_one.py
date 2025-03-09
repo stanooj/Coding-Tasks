f8a4bad6050ce899dd15d0ee523cae8e2318ad4a
stanooj <tanoojbobby143@gmail.com>
2025-03-09 17:15:32 +0530

    Add files via upload

---
+++ Packages/class_one.py
@@ -0,0 +1,6 @@
+class ClassOne:
+    def __init__(self):
+        print("ClassOne Constructor Called")
+
+    def method_one(self):
+        print("Method of ClassOne Called")
---
+++ Packages/main.py
@@ -0,0 +1,8 @@
+
+from Packages import ClassOne, ClassTwo
+
+if __name__ == "__main__":
+    obj1 = ClassOne()  
+    obj1.method_one()  
+    obj2 = ClassTwo() 
+    obj2.method_two() 
